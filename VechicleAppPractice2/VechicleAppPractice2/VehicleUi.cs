56e97dfa0e088475d35c13a296e07fac9e72bbf1
prantoshon <prantoshon14@gmail.com>
2019-10-15 22:13:24 +0600

    done

---
+++ VechicleAppPractice2/VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        VehiclesClass vehicles = new VehiclesClass();
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            vehicles.name = vehicleNameTextBox.Text;
+            vehicles.reg = regNoTextBox.Text;
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            vehicles.speed.Add(Convert.ToDouble(speedTextBox.Text));
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(vehicleNameTextBox.Text))
+            {
+                MessageBox.Show("Name is Empty");
+                return;
+            }
+            if (String.IsNullOrEmpty(regNoTextBox.Text))
+            {
+                MessageBox.Show("Reg  is Empty");
+                return;
+            }
+
+
+            maxSpeedTextBox.Text = vehicles.MaxCalculate().ToString();
+            minSpeedTextBox.Text = vehicles.MinCalculate().ToString();
+            averageSpeedTextBox.Text = vehicles.AvgCalculate().ToString();
+        }
+    }
+}
---
+++ VechicleAppPractice2/VechicleAppPractice2/VehiclesClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    public  class VehiclesClass
+    {
+        public string name { get; set; }
+        public string reg { get; set; }
+
+        public  List<double> speed = new List<double>();
+
+        public Double MaxCalculate()
+        {
+            return speed.Max();
+        }
+        public Double MinCalculate()
+        {
+            return speed.Min();
+        }
+        public Double AvgCalculate()
+        {
+            return speed.Average();
+        }
+
+
+
+    }
+}
